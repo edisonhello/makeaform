7e309ed99d560d6cb046a79dfeab45b31a4cee8d
edisonhello <edisonhello@hotmail.com>
2015-12-22 12:40:49 +0800

    1

--- app.js
+++ app.js
@@ -15,10 +15,20 @@ app.get('/',function(req,res){
 });
 
 app.post('/',function(req,res){
-    res.send(req.body);
-    res.end();  
+	if(req.body.pass!=req.body.pass2){
+		res.sendFile(__dirname+'form_pass.html',function(){res.end()})
+		if(req.body.birth==""){
+   			res.sendFile(__dirname+'form_birth.html',function(){res.end()})
+   	   }else{
+   		res.sendFile(__dirname+'done.html',function(){res.end()})
+       }
+    }
+    else{
+    	res.sendFile(__dirname+'done.html',function(){res.end()}) 
+    }
+
+app.get('/list',function(req,res){
+//    res.sendFile(__dirname+'/list.html',function(){res.end();})
 });
-
-
 
 app.listen(3000);
